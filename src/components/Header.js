7e4bc8ed5df3bc080e99fb2b916524d14ec4dbe4
Arya Krishna <dr.aryakris@outlook.com>
2023-03-04 18:23:33 -0800

    created react app, components

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Button} from './Button'
+
+export const Header = () => {
+  return (
+    <div>
+          <header className='header'>
+              <h1>Task Tacker</h1>
+              <Button />
+
+          </header>
+    </div>
+  )
+}
+
+
