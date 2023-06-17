072eff69f3152481e9edc53cd753d885e4ff887b
Arya Krishna <dr.aryakris@outlook.com>
2023-06-16 19:58:54 -0700

    installed json server,in the db.json file added the task object

--- src/App.js
+++ src/App.js
@@ -61,14 +61,4 @@ function addTask(task){
 export default App;
 
 
-// import { useState } from "react";
-// import ReactDOM from "react-dom/client";
 
-// function FavoriteColor() {
-//   const [color, setColor] = useState("red");
-
-//   return <h1>My favorite color is {color}!</h1>
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<FavoriteColor />);
