2f4520e78f50636c54d4f23e746ebec44ed9df90
Arya Krishna <dr.aryakris@outlook.com>
2023-06-22 21:02:10 -0700

    moved the data from App.js to db.json

--- src/App.js
+++ src/App.js
@@ -22,26 +22,7 @@ function addTask(task){
 }
 
 
-  const [data, setTasks] = useState( [
-    {
-      "id": 1,
-      "text": "Doctors Appointment",
-      "day": "Feb 5th at 2.30pm",
-      "reminder": true
-    },
-    {
-      "id": 2,
-      "text": "Meeting at School",
-      "day": "Feb 6th at 1.30pm",
-      "reminder": false
-    },
-    {
-      "id": 3,
-      "text": "Food Shopping",
-      "day": "Feb 5th at 2.30pm",
-      "reminder": false
-    }
-  ])
+  const [data, setTasks] = useState( [] )
 
   const deleteTask = (id) => {
   //   setTasks(tasks.filter((task) => task.id!==id))
